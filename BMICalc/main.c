8aa92ccfdb6d4edf3a755af6527b18eda4256b5d
Kate Owens <kowens711@gmail.com>
2017-04-19 10:49:36 -0400

    wrote typedef struct

--- BMICalc/main.c
+++ BMICalc/main.c
@@ -8,8 +8,24 @@
 
 #include <stdio.h>
 
+typedef struct {
+    float heightInMeters;
+    float weightInKilos;
+} Person;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    Person mikey;
+    mikey.heightInMeters = 1.7;
+    mikey.weightInKilos = 96;
+    
+    Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("mikey is %.2f meters tall\n", mikey.heightInMeters);
+    printf("mikey weighs %d kilograms\n", mikey.weightInKilos);
+    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    
     return 0;
 }
